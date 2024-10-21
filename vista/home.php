e5a7becd341191e8b23d6d740edf9f07c5e323f4
EliasMiranda02 <9613266784az@gmail.com>
2024-10-21 00:26:03 -0600

    Add files via upload

---
+++ vista/home.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chiapas Viajero</title>
+    <link rel = "icon" type = "image/ico" href = "img/Icono.ico" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+    crossorigin="anonymous">
+    <link rel = "stylesheet" href = "css/home.css">
+    <style>
+        .carrusel-contain {
+            background-image: url('./img/home/Fondo.png');
+            background-size: cover; 
+            background-position: center; 
+            height: 573px; 
+            width: 100%; 
+        }
+    </style>
+</head>
+<body>
+    <section class="header">
+    <nav class="navbar">
+            <a href="#">SERVICIOS</a>
+            <a href="#">HISTORIAL</a>
+            <img src="img/Logo.png" alt="Logo" class="logo">
+            <a href="#">NOTIFICACIONES</a>
+            <a href="#">ACERCA DE</a>
+        </nav>
+        <img src="img/home/Banner.gif" alt="Banner" class="banner">
+
+        <a href="#" class="reserva-button">
+            <span class="inner-button">¡Reserva Aquí!</span>
+        </a>
+
+        <a href="#" class="access-button">ACCESO</a>
+    </section>
+    <!-- Cuerpo de la página -->
+    <main>
+        <br><br><b><p class="title">¿POR QUÉ RESERVAR EN CHIAPAS VIAJERO?</p></b>
+        <section class="image-container">
+            <div class="image-item">
+                <img src="img/home/RecursoA.png" alt="Atención al Cliente">
+                <br><br><p class="subtitle">Atención al Cliente 24/7</p>
+                <p class="text">Estamos disponibles para resolver dudas y brindar asistencia
+                    antes, durante y después del viaje.</p>
+            </div>
+            <div class="image-item">
+                <img src="img/home/RecursoB.png" alt="Seguridad y Confianza">
+                <br><br><p class="subtitle">Seguridad y Confianza</p>
+                <p class="text">No solo estás reservando un viaje, estás asegurando una
+                    experiencia tranquila, segura y memorable con nosotros.</p>
+            </div>
+            <div class="image-item">
+                <img src="img/home/RecursoC.png" alt="¡Viajar Más, Gastar Menos!">
+                <br><br><p class="subtitle">¡Viajar Más, Gastar Menos!</p>
+                <p class="text">Con nuestras ofertas exclusivas y precios accesibles, ¡tu
+                    próxima aventura solo está al alcance de tus manos!</p>
+            </div>
+            <div class="image-item">
+                <img src="img/home/RecursoD.png" alt="La Voz de Nuestros Viajeros">
+                <br><br><p class="subtitle">La Voz de Nuestros Viajeros</p>
+                <p class="text">Las historias de nuestros clientes son la mejor prueba de que,
+                    con nosotros, tu viaje será inolvidable y excepcional.</p>
+            </div>
+        </section><br>
+
+        <section class="frase">
+            <p class="frase-text">"Con <b class="resaltador-text">Chiapas Viajero</b>, transformamos
+                tus sueños en realidades: disfruta de experiencias inolvidables guiadas por expertos
+                apasionados con ofertas exclusivas a precios irresistibles que desearás regresar.</p>
+            <br><p class="frase-text"><b>¡Únete a nosotros y descubre Chiapas a través de auténticas
+                exploraciones y recuerdos que perdurarán para siempre!"</b></p>
+        </section>
+
+        <section class="carrusel-contain"><br><br>
+            <b><p class="title">NUESTROS SERVICIOS</p></b><br>
+    
+            <div class="carrusel-container">
+                <button class="prev"></button>
+                <div class="carrusel">
+                    <div class="imagen">
+                        <div class="img">
+                            <a>
+                                <img src="./img/home/FotoA.png" alt="San Cristóbal de las Casas">
+                            </a>
+                        </div>
+                        <div class="img"><img src="./img/home/FotoB.png" alt="Ocosingo"></div>
+                        <div class="img"><img src="./img/home/FotoC.png" alt="Palenque"></div>
+                        <div class="img"><img src="./img/home/FotoD.png" alt="Tumbalá"></div>
+                        <div class="img"><img src="./img/home/FotoE.png" alt="Tonalá"></div>
+                        <div class="img"><img src="./img/home/FotoF.png" alt="Tapachula"></div>
+                        <div class="img"><img src="./img/home/FotoG.png" alt="Chiapa de Corzo"></div>
+                        <div class="img"><img src="./img/home/FotoH.png" alt="La Trinitaria"></div>
+                        <div class="img"><img src="./img/home/FotoI.png" alt="Tuxtla Gutiérrez"></div>
+                    </div>
+                </div>
+                <button class="next"></button>
+            </div>
+        </section><br><br>
+
+        <section class="map">
+            <div class="map-header">
+                <img src="img/home/Cuadrito.png" alt="Viñeta">
+                <p class="map-title">DESCUBRE NUEVOS MUNDOS</p>
+            </div>
+            <br><br><p class="map-description">Con nuestro conocimiento local, te ofrecemos viajes
+                cuidadosamente seleccionados para garantizarte una experiencia inolvidable. Consulta
+                el mapa para ver las ubicaciones de los destinos que ofrecemos.</p>
+        </section><br><br>
+        <img src="img/home/Mapa.png" alt="Mapa de Chiapas" class="map-image">
+        <div class="linea-divisora"></div><br>
+
+        <section class="questions-container">
+            <b><p class="title">PREGUNTAS FRECUENTES</p></b><br><br>
+
+            <div class="container-questions">
+                <div class="questions">
+                    <div class="columna">
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Qué es Chiapas Viajero?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>Chiapas Viajero es una operadora turística dedicada a
+                                ofrecer experiencias únicas e inolvidables en el hermoso estado de Chiapas.
+                                Nos especializamos en crear itinerarios que resaltan la riqueza cultural,
+                                natural e histórica de la región.<br> Nuestro equipo de expertos está
+                                comprometido a brindarte un servicio excepcional y asegurarse de que
+                                disfrutes al máximo cada momento de tu viaje.</p>
+                            <hr>
+                        </div>
+
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Qué hago si mi vuelo se retrasa o  cancela?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>En caso de que tu vuelo se retrase o cancele, nuestra
+                                prioridad es asegurarnos de que tu viaje siga siendo lo más cómodo y
+                                fluido posible. Nuestro equipo estará disponible para asistirte en la
+                                reprogramación de traslados y actividades. Además te mandaremos otras
+                                opciones que se adecuen a tus gustos para no perjudicar tus vacaciones.</p>
+                            <hr>
+                        </div>
+
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Mi vuelo se reserva con Chiapas Viajero?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>Sí, trabajamos con nuestra aerolínea “Chiapas Volante”
+                                para ofrecerte servicios de vuelo. Esto nos permite coordinar tu itinerario
+                                de manera integral, asegurando una experiencia fluida desde el inicio hasta
+                                el final de tu viaje. Si reservas tu vuelo con nosotros, nuestro equipo
+                                estará disponible para asegurarse de que todo esté organizado, brindándote
+                                tranquilidad y confianza en tu viaje.</p>
+                            <hr>
+                        </div>
+
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Cuál es la política de reembolso?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>Nuestra política de reembolso permite cancelaciones
+                                hasta 48 horas antes de tu vuelo. Si decides cancelar después de ese plazo,
+                                se aplicará un cargo del 20% del total de tu reserva, ya que algunos costos
+                                son no reembolsables.</p>
+                            <hr>
+                        </div>
+                    </div>
+
+                    <div class="columna">
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Puedo modificar o cancelar mi reserva?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>Sí, entendemos que los planes pueden cambiar. En Chiapas
+                                Viajero, ofrecemos opciones flexibles para modificar o cancelar tu reserva.
+                                <br>
+                                Si necesitas realizar cambios, simplemente contáctanos con al menos 48 horas
+                                de anticipación y haremos nuestro mejor esfuerzo para acomodar tus
+                                solicitudes.</p><hr>
+
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Qué pasará después de mi vuelo?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>Una vez que llegues a Chiapas, nuestro equipo estará
+                                listo para recibirte y acompañarte en tu aventura. Dependiendo de tu
+                                itinerario, te trasladaremos a tu alojamiento o al primer destino de tu
+                                recorrido. Nuestro compromiso es brindarte una experiencia inolvidable y
+                                sin preocupaciones desde el momento de tu llegada.
+                            </p>
+                            <hr>      
+                        </div> 
+
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Cómo consulto el estado de mi vuelo?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>Puedes consultar el estado de tu reserva a través de
+                                nuestra plataforma. Contamos con un apartado de historial donde podrás
+                                ver todos los detalles de tu reserva, incluidos los estados y cualquier
+                                actualización relevante.</p>
+                            <hr>      
+                        </div>  
+
+                        <div class="question">
+                            <div class="container-question">
+                                <p class="question_title">¿Qué hago si no recibo confirmación de pago?</h4>
+                                <span class="question_arrow">
+                                    <img src="./img/home/IconoAbajo.png" class="question_img">
+                                </span>
+                            </div>
+                            <p class="answer"><br>Si no has recibido la confirmación de tu reserva,
+                                por favor, verifica tu carpeta de spam o correo no deseado. Si aún no
+                                encuentras el correo, contáctanos de inmediato y nuestro equipo estará
+                                encantado de asegurarse de que tu reserva esté confirmada.
+                            </p>
+                            <hr>      
+                        </div>  
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main><br><br>
+
+    <section class="footer">
+    <div class="footer-container">
+            <div class="logo">
+                <img src="img/Logo.png" alt="Logo - Chiapas Viajero">
+            </div>
+            <div class="social-media">
+                <ul>
+                    <li><a href="#"><img src="img/home/Facebook.png" alt="Facebook"></a></li>
+                    <li><a href="#"><img src="img/home/Pinterest.png" alt="Pinterest"></a></li>
+                    <li><a href="#"><img src="img/home/Instagram.png" alt="Instagram"></a></li>
+                    <li><a href="#"><img src="img/home/TikTok.png" alt="TikTok"></a></li>
+                </ul>
+            </div> 
+        </div><br>
+        <div class="footer-menu">
+            <ul>
+                <li><a href="#"><span>Términos y Condiciones</span></a></li>
+                <li><a href="#"><span>Política de Privacidad</span></a></li>
+                <li><a href="#"><span>Manual de Usuario</span></a></li>
+                <li><a href="#"><span>Testimonios de Clientes</span></a></li>
+            </ul>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+    <script src="js/home_bar.js"></script>
+    <script src="js/home_carrusel.js"></script>
+    <script src="js/home_questions.js"></script>
+</body>
+</html>
